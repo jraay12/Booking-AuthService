79ef223185bd5a1745cb49bb5f3fc6e1bcd2cfad
John Ray <johnraycaete@gmail.com>
2026-04-19 06:33:22 +0800

    add new method in user repository interface findById and update

--- src/modules/User/types/user-repository.interface.ts
+++ src/modules/User/types/user-repository.interface.ts
@@ -3,4 +3,6 @@ import { User } from "@prisma/client";
 export interface UserRepository {
   create(user: { email: string; password: string, first_name: string, last_name: string }): Promise<any>;
   findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
+  update(user: { email: string, first_name: string, last_name: string }, user_id: string): Promise<User>;
 }
