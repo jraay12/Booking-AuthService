0e014b2e1943a724df4db2c370d98d38e9fabdfc
John Ray <johnraycaete@gmail.com>
2026-04-18 21:41:30 +0800

    add test for login user

--- tests/unit/auth.service.test.ts
+++ tests/unit/auth.service.test.ts
@@ -1,11 +1,12 @@
 import { AuthService } from "./../../src/modules/auth.service";
 import { UserRepository } from "../../src/modules/types/user-repository.interface";
 import { BcryptService } from "../../src/modules/types/bcrypt.interface";
-
+import { JwtService } from "../../src/modules/types/jwt.interface";
 describe("Auth Service", () => {
   let authService: AuthService;
   let userRepository: UserRepository;
   let bcryptService: BcryptService;
+  let jwtService: JwtService;
 
   beforeEach(() => {
     userRepository = {
@@ -18,7 +19,12 @@ describe("Auth Service", () => {
       hash: jest.fn().mockResolvedValue("hash_password"),
     };
 
-    authService = new AuthService(userRepository, bcryptService);
+    jwtService = {
+      sign: jest.fn().mockReturnValue("jwt-token"),
+      verify: jest.fn(),
+    };
+
+    authService = new AuthService(userRepository, bcryptService, jwtService);
   });
 
   it("should be defined", async () => {
@@ -26,6 +32,14 @@ describe("Auth Service", () => {
   });
 
   it("should hash password when registering user", async () => {
+    (userRepository.create as jest.Mock).mockResolvedValue({
+      id: "1",
+      email: "johnraycaete@gmail.com",
+      first_name: "johnray",
+      last_name: "canete",
+      password: "hash_password",
+    });
+
     await authService.register({
       email: "johnraycaete@gmail.com",
       first_name: "johnray",
@@ -36,6 +50,14 @@ describe("Auth Service", () => {
   });
 
   it("should save user with hashed password", async () => {
+    (userRepository.create as jest.Mock).mockResolvedValue({
+      id: "1",
+      email: "johnraycaete@gmail.com",
+      first_name: "johnray",
+      last_name: "canete",
+      password: "hash_password",
+    });
+
     await authService.register({
       email: "johnraycaete@gmail.com",
       first_name: "johnray",
@@ -85,6 +107,54 @@ describe("Auth Service", () => {
     });
 
     expect(result).toEqual(createdUser);
+  });
 
+  it("should fail if email not found", async () => {
+    (userRepository.findByEmail as jest.Mock).mockResolvedValue(null);
+
+    await expect(
+      authService.login({
+        email: "notfound@gmail.com",
+        password: "123456",
+      }),
+    ).rejects.toThrow("User not found");
+  });
+
+  it("should fail if password does not match", async () => {
+    (userRepository.findByEmail as jest.Mock).mockResolvedValue({
+      id: "1",
+      email: "john@gmail.com",
+      password: "hashed_password",
+    });
+
+    (bcryptService.compare as jest.Mock).mockResolvedValue(false);
+
+    await expect(
+      authService.login({
+        email: "john@gmail.com",
+        password: "wrongpassword",
+      }),
+    ).rejects.toThrow("Invalid credentials");
+  });
+
+  it("should return JWT token when login is successful", async () => {
+    const user = {
+      id: "1",
+      email: "john@gmail.com",
+      password_hash: "hashedPassword",
+    };
+
+    (userRepository.findByEmail as jest.Mock).mockResolvedValue(user);
+    (bcryptService.compare as jest.Mock).mockResolvedValue(true);
+    (jwtService.sign as jest.Mock).mockReturnValue("jwt-token");
+
+    const result = await authService.login({
+      email: "john@gmail.com",
+      password: "123456",
+    });
+
+    expect(result).toEqual({
+      access_token: "jwt-token"
+    })
   });
 });
