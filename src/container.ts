e66edd93713174cb3267b3dfd53391a2b9fd9b56
John Ray <johnraycaete@gmail.com>
2026-04-18 21:42:58 +0800

    wire the jwt service to the authService

--- src/container.ts
+++ src/container.ts
@@ -3,14 +3,15 @@ import { AuthService } from "./modules/auth.service";
 import { AuthRepositoryImpl } from "./modules/auth.repository";
 import { BcryptImpl } from "./utils/bcrypt";
 import {prisma} from "./utils/prisma"
+import { JwtServiceImpl } from "./utils/jwt";
 
 // Repositories
 const userRepo = new AuthRepositoryImpl(prisma)
 
 // other services
 const bcrypt = new BcryptImpl()
-
+const jwt = new JwtServiceImpl(process.env.SECRET_TOKEN!)
 // services
-const authService = new AuthService(userRepo, bcrypt)
+const authService = new AuthService(userRepo, bcrypt, jwt)
 
 export const authController = new AuthController(authService)
