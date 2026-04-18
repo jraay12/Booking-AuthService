9ad007bc991f5d8af760e8dfdfb42f16c9485fd4
John Ray <johnraycaete@gmail.com>
2026-04-19 06:35:23 +0800

    wire everything

--- src/container.ts
+++ src/container.ts
@@ -1,17 +1,21 @@
 import { AuthController } from "./modules/Auth/auth.controller";
 import { AuthService } from "./modules/Auth/auth.service";
-import { AuthRepositoryImpl } from "./modules/User/auth.repository";
+import { UserRepositoryImpl } from "./modules/User/user.repository";
 import { BcryptImpl } from "./utils/bcrypt";
 import { prisma } from "./utils/prisma";
 import { JwtServiceImpl } from "./utils/jwt";
-
+import { UserController } from "./modules/User/user.controller";
+import { UserService } from "./modules/User/user.service";
 // Repositories
-const userRepo = new AuthRepositoryImpl(prisma);
+const userRepo = new UserRepositoryImpl(prisma);
 
 // other services
 const bcrypt = new BcryptImpl();
 const jwt = new JwtServiceImpl(process.env.SECRET_TOKEN!);
+
 // services
 const authService = new AuthService(userRepo, bcrypt, jwt);
+const userService = new UserService(userRepo)
 
 export const authController = new AuthController(authService);
+export const userController = new UserController(userService)
