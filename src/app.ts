5a6db89be45e3d7a193f636a4eeae08d7f1b9f60
John Ray <johnraycaete@gmail.com>
2026-04-19 06:35:33 +0800

    register the user routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { Request, Response } from "express";
-import { authController } from "./container";
+import { authController, userController } from "./container";
 import authRoutes from "./modules/Auth/auth.routes";
+import userRoutes from "./modules/User/user.routes";
 import { GlobalErrorHandler } from "./middleware/GlobalErrorHandler";
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRoutes(authController));
+app.use("/user", userRoutes(userController));
+
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({
