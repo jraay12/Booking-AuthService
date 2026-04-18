b21fd482750058f53aadafebb37689104498007d
John Ray <johnraycaete@gmail.com>
2026-04-19 07:33:11 +0800

    change to Partial the update

--- src/modules/User/types/user-repository.interface.ts
+++ src/modules/User/types/user-repository.interface.ts
@@ -4,5 +4,6 @@ export interface UserRepository {
   create(user: { email: string; password: string, first_name: string, last_name: string }): Promise<any>;
   findByEmail(email: string): Promise<User | null>;
   findById(id: string): Promise<User | null>;
-  update(user: { email: string, first_name: string, last_name: string }, user_id: string): Promise<User>;
+  update(user: Partial<User>, user_id: string): Promise<User>;
+
 }
