9c36f2ce396294c27c4e76267b9267f23255cebd
John Ray <johnraycaete@gmail.com>
2026-04-18 21:42:24 +0800

    add login method for auth service

--- src/modules/auth.service.ts
+++ src/modules/auth.service.ts
@@ -1,11 +1,14 @@
 import { ConflictError } from "../shared/ConflictError";
 import { BcryptService } from "./types/bcrypt.interface";
 import { UserRepository } from "./types/user-repository.interface";
-
+import { JwtService } from "./types/jwt.interface";
+import { NotFoundError } from "../shared/NotFoundError";
+import { UnAuthorizedError } from "../shared/UnAuthorizedError";
 export class AuthService {
   constructor(
     private userRepository: UserRepository,
     private bcrypt: BcryptService,
+    private jwt: JwtService
   ) {}
 
   async register(dto: {
@@ -28,5 +31,19 @@ export class AuthService {
     const { password, ...result } = user;
 
     return result
+  }
+
+  async login(dto: {email: string, password: string}){
+    const user = await this.userRepository.findByEmail(dto.email)
+
+    if(!user) throw new NotFoundError("User not found")
+
+    const isMatch = await this.bcrypt.compare(dto.password, user.password_hash)
+
+    if(!isMatch) throw new UnAuthorizedError("Invalid credentials")
+
+    const access_token = this.jwt.sign({id: user.id})
+
+    return {access_token}
   }
 }
