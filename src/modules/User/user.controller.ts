1c8c3d377a619be28a55dcb6904bc561c1b33efc
John Ray <johnraycaete@gmail.com>
2026-04-19 08:07:57 +0800

    add endpoint for deactivate user

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -8,14 +8,29 @@ export class UserController {
   update = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const InputBody: UpdateUserDTO = req.body as UpdateUserDTO;
-      const InputParams = req.params as {user_id: string} ;
-      const result = await this.userService.update(InputBody, InputParams.user_id)
+      const InputParams = req.params as { user_id: string };
+      const result = await this.userService.update(
+        InputBody,
+        InputParams.user_id,
+      );
       res.status(201).json({
         message: "Successfully update user",
         data: result,
       });
     } catch (error) {
       next(error);
+    }
+  };
+
+  deactivate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const inputParams = req.params as {user_id: string}
+      const result = await this.userService.deactivate(inputParams.user_id)
+      res.status(200).json({
+        message:`Successfully deactivate user ${result.first_name} ${result.last_name}`,
+      })
+    } catch (error) {
+      next(error)
     }
   };
 }
