41e3854a2ff51191e4e2417ba69bda440ba3a92b
John Ray <johnraycaete@gmail.com>
2026-04-19 08:26:19 +0800

    add new method activate user

--- src/modules/User/user.service.ts
+++ src/modules/User/user.service.ts
@@ -4,25 +4,36 @@ import { UpdateUserDTO } from "./dto/user.dto";
 import { UserRepository } from "./types/user-repository.interface";
 
 export class UserService {
-  constructor(private userRepo: UserRepository){}
+  constructor(private userRepo: UserRepository) {}
 
-  async update(dto: UpdateUserDTO, user_id: string){
-    const existingUser = await this.userRepo.findById(user_id)
+  async update(dto: UpdateUserDTO, user_id: string) {
+    const existingUser = await this.userRepo.findById(user_id);
 
-    if(!existingUser) throw new NotFoundError("User not found")
+    if (!existingUser) throw new NotFoundError("User not found");
 
-    const {password_hash, ...safe} = await this.userRepo.update(dto, user_id)
+    const { password_hash, ...safe } = await this.userRepo.update(dto, user_id);
 
-    return safe
+    return safe;
   }
 
-  async deactivate(user_id: string){
-   const user = await this.userRepo.findById(user_id)
+  async deactivate(user_id: string) {
+    const user = await this.userRepo.findById(user_id);
 
-   if(!user) throw new NotFoundError("User not found")
+    if (!user) throw new NotFoundError("User not found");
 
-  if(!user.is_active) throw new BadRequestError("User is a already deactivated")
+    if (!user.is_active)
+      throw new BadRequestError("User is a already deactivated");
 
-  return await this.userRepo.update({is_active: false}, user_id)
+    return await this.userRepo.update({ is_active: false }, user_id);
+  }
+
+  async activate(user_id: string) {
+    const user = await this.userRepo.findById(user_id);
+
+    if (!user) throw new NotFoundError("User not found");
+
+    if (user.is_active) throw new BadRequestError("User already activated");
+
+    return await this.userRepo.update({ is_active: true }, user_id);
   }
 }
