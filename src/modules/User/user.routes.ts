a46f78dc45f519f3b4ed1238be5a0acc51b82ed8
John Ray <johnraycaete@gmail.com>
2026-04-19 08:26:39 +0800

    add new patch route

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -1,12 +1,21 @@
-import { UserController } from './user.controller';
+import { UserController } from "./user.controller";
 import { Router } from "express";
-import { authMiddleware } from '../../middleware/auth.middleware';
-import { JwtService } from '../Auth/types/jwt.interface';
+import { authMiddleware } from "../../middleware/auth.middleware";
+import { JwtService } from "../Auth/types/jwt.interface";
 const userRoutes = (userController: UserController, jwtService: JwtService) => {
   const router = Router();
 
   router.patch("/:user_id", authMiddleware(jwtService), userController.update);
-  router.patch("/:user_id/deactivate", authMiddleware(jwtService), userController.deactivate);
+  router.patch(
+    "/:user_id/deactivate",
+    authMiddleware(jwtService),
+    userController.deactivate,
+  );
+  router.patch(
+    "/:user_id/activate",
+    authMiddleware(jwtService),
+    userController.activate,
+  );
 
   return router;
 };
